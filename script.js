60b4a1cd8bd05734af2026b215fdf8f8fde9d332
Nathan Hadley <nathanhadley.pigeon@gmail.com>
2024-12-05 13:59:11 +0000

    Add reset button

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let savedLocation = localStorage.getItem("location");
-savedLocation = savedLocation.split(",");
-savedLocation = [Number(savedLocation[0]), Number(savedLocation[1])];
-console.log(savedLocation);
+if (savedLocation != null) {
+  savedLocation = savedLocation.split(",");
+  savedLocation = [Number(savedLocation[0]), Number(savedLocation[1])];
+  console.log(savedLocation);
+}
 
-const map = L.map('map').setView(savedLocation || [0.628118,-50.199234], localStorage.getItem("zoom") || 13);
+const map = L.map('map').setView(savedLocation || [-3.309120512165984,-60.220870971679695], localStorage.getItem("zoom") || 14);
 L.tileLayer('https://clarity.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   maxZoom: 22,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'

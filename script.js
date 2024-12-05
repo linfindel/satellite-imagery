2bb411c491b0e6615689553e2259d6ffab1237bf
Nathan Hadley <nathanhadley.pigeon@gmail.com>
2024-12-05 13:30:54 +0000

    Added attribution

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([0.628118,-50.199234], 14);
 L.tileLayer('https://clarity.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   maxZoom: 22,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
